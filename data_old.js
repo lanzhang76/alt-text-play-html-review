8eaf69028c1ed52dd793ec30502b8c370d917bf4
Lan Zhang <lanzhangwork@gmail.com>
2022-03-22 23:36:53 -0400

    update data format and article display interaction

---
+++ data_old.js
@@ -0,0 +1,23 @@
+const data = {
+  01: {
+    url: "https://www.google.com/search?q=bread&tbm=isch&ved=2ahUKEwia3MDIztf2AhWOGVkFHQ9hCSkQ2-cCegQIABAA&oq=bread&gs_lcp=CgNpbWcQDFAAWABgAGgAcAB4AIABAIgBAJIBAJgBAKoBC2d3cy13aXotaW1n&sclient=img&ei=ZqY4YpqRGY6z5NoPj8KlyAI&bih=668&biw=1186",
+    img: "./images/01.png",
+    alt: "Here you can find a variety of delicious, fluffly and wholesome bread.",
+    haiku: "<p>hello hello</p><p>hello hello</p><p>hello hello hello</p>",
+    caption: "et dolore magnam aliquam quaerat voluptatem. nima veniam",
+  },
+  02: {
+    url: "https://www.google.com/search?q=bread&tbm=isch&ved=2ahUKEwia3MDIztf2AhWOGVkFHQ9hCSkQ2-cCegQIABAA&oq=bread&gs_lcp=CgNpbWcQDFAAWABgAGgAcAB4AIABAIgBAJIBAJgBAKoBC2d3cy13aXotaW1n&sclient=img&ei=ZqY4YpqRGY6z5NoPj8KlyAI&bih=668&biw=1186",
+    img: "./images/01.png",
+    alt: "Here you can find a variety of delicious, fluffly and wholesome bread.",
+    haiku: "<p>hello hello</p><p>hello hello</p><p>hello hello hello</p>",
+    caption: "et dolore magnam aliquam quaerat voluptatem. nima veniam",
+  },
+  03: {
+    url: "https://www.google.com/search?q=bread&tbm=isch&ved=2ahUKEwia3MDIztf2AhWOGVkFHQ9hCSkQ2-cCegQIABAA&oq=bread&gs_lcp=CgNpbWcQDFAAWABgAGgAcAB4AIABAIgBAJIBAJgBAKoBC2d3cy13aXotaW1n&sclient=img&ei=ZqY4YpqRGY6z5NoPj8KlyAI&bih=668&biw=1186",
+    img: "./images/01.png",
+    alt: "Here you can find a variety of delicious, fluffly and wholesome bread.",
+    haiku: "<p>hello hello</p><p>hello hello</p><p>hello hello hello</p>",
+    caption: "et dolore magnam aliquam quaerat voluptatem. nima veniam",
+  },
+};
--- main.js
+++ main.js
@@ -15,58 +15,76 @@ function displayData() {
     newItem.setAttribute("id", `haiku${item}`);
 
     //create URL around the image
-    let newURL = document.createElement("a");
-    newURL.href = altItem.url;
-    newURL.target = "_blank";
+    // let newURL = document.createElement("a");
+    // newURL.href = altItem.url;
+    // newURL.target = "_blank";
 
     //create img + add class
-    let newImg = new Image();
-    newImg.src = altItem.img;
-    newImg.alt = altItem.alt;
-    newImg.classList.add("backdrop");
+    // let newImg = new Image();
+    // newImg.src = altItem.img;
+    // newImg.alt = altItem.alt;
+    // newImg.classList.add("backdrop");
 
     //create haiku poem + add class
     let newHaiku = document.createElement("div");
     newHaiku.classList.add("haiku");
     newHaiku.innerHTML = altItem.haiku;
 
-    //create caption for each haiku display
-    let newCaption = document.createElement("p");
-    newCaption.classList.add("caption");
-    newCaption.innerText = altItem.caption;
-
     mainArea.appendChild(newItem);
     newURL.appendChild(newImg);
     newItem.appendChild(newURL);
     newItem.appendChild(newHaiku);
-    newItem.appendChild(newCaption);
   }
 }
 
-displayData();
+// displayData();
+
+// const items = document.querySelectorAll(".item");
+// gsap.set(".haiku p", { opacity: 0, scale: 1.2 });
+// items.forEach((item) => {
+//   let tl = gsap.timeline();
+//   tl.to(`#${item.id}`, 0.5, {
+//     scrollTrigger: {
+//       trigger: `#${item.id}`,
+//       start: "center bottom",
+//       end: "center center",
+//       scrub: true,
+//       //   markers: true,
+//     },
+//     y: "-=100",
+//     opacity: 1,
+//   }).to(`#${item.id} .haiku p`, 0.5, {
+//     scrollTrigger: {
+//       trigger: `#${item.id}`,
+//       start: "center bottom+=200px",
+//       end: "bottom center+=200px",
+//       scrub: true,
+//     },
+//     opacity: 1,
+//     scale: 1,
+//   });
+// });
+
+data.forEach((item) => {
+  //create span tag
+  let newItem = document.createElement("span");
+  newItem.classList.add("item");
+  newItem.setAttribute("id", `${data.indexOf(item)}`);
+  newItem.innerText = `${item.alt}, `;
+  mainArea.appendChild(newItem);
+});
 
 const items = document.querySelectorAll(".item");
-gsap.set(".haiku p", { opacity: 0, scale: 1.2 });
+const bgImg = document.querySelector("#bgImg");
 items.forEach((item) => {
-  let tl = gsap.timeline();
-  tl.to(`#${item.id}`, 0.5, {
-    scrollTrigger: {
-      trigger: `#${item.id}`,
-      start: "center bottom -=200px",
-      end: "bottom center",
-      scrub: true,
-      //   markers: true,
-    },
-    y: "-=120",
-    opacity: 1,
-  }).to(`#${item.id} .haiku p`, 0.5, {
-    scrollTrigger: {
-      trigger: `#${item.id}`,
-      start: "center bottom+=200px",
-      end: "bottom center+=200px",
-      scrub: true,
-    },
-    opacity: 1,
-    scale: 1,
+  item.addEventListener("mouseover", () => {
+    item.classList.add("active");
+    let src = data[item.id].src;
+    bgImg.src = src;
+  });
+
+  item.addEventListener("mouseleave", () => {
+    item.classList.remove("active");
+    bgImg.src = "";
   });
 });

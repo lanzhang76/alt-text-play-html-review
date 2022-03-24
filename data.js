b9ea6a227498bf49e966a671ef187fa37c3a0abc
Lan Zhang <lanzhangwork@gmail.com>
2022-03-23 20:56:55 -0400

    merge

--- data.js
+++ data.js
@@ -11,12 +11,10 @@ const data = [
     alt: "Toad ceremony participants dancing in Huntsville, Texas. The organizer of the event charges $250 for a ceremony",
     src: "https://static01.nyt.com/images/2022/03/09/us/00toad08/merlin_203378163_fa83b9c1-ba08-4d14-80bf-63fdb5edb3e9-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
   },
-
   {
     alt: "Brooke Tarrer, center, of the Universal Shamans of the New Tomorrow church, smoking a tobacco cigarette during preparation for a toad medicine ceremony",
     src: "https://static01.nyt.com/images/2022/03/09/us/00toad13/merlin_203377266_0b95a31e-ea6b-445f-82e6-ce93cd0ca894-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
   },
-
   {
     alt: "A ceremony participant after inhaling toad secretion smoke. A substance found in the toad toxins produces an intense experience generally lasting 15 to 30 minutes",
     src: "https://static01.nyt.com/images/2022/03/09/us/00toad12/merlin_203377740_ffaba7f9-9114-4bd5-88b8-0f608f8d9916-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
@@ -26,14 +24,13 @@ const data = [
     src: "https://static01.nyt.com/images/2022/03/09/us/00toad09/merlin_203377623_294c5d78-4fa5-47df-9254-5d01092b7906-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
   },
   {
-    alt: "Robert Villa, president of the Tucson Herpetological Society, said the surging demand for the Sonoran desert toad could lead its population to plummet.  ",
+    alt: "Robert Villa, president of the Tucson Herpetological Society, said the surging demand for the Sonoran desert toad could lead its population to plummet",
     src: "https://static01.nyt.com/images/2022/03/09/us/00toad11/merlin_203412855_bfba309d-071c-47c5-b4af-a0f8a5de557c-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
   },
   {
     alt: "The toad is primarily found in the Sonoran desert in parts of the southwestern United States and northwestern Mexico",
     src: "https://static01.nyt.com/images/2022/03/09/us/00toad03/merlin_203413020_8e0a22e1-1e84-46e0-b9ca-d517c0de896b-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
   },
-
   {
     alt: "The toad has a remarkably long life span that can reach 20 years",
     src: "https://static01.nyt.com/images/2022/03/09/us/00toad02/merlin_203413095_d8ae7a59-4939-4383-a2fe-4d08c03490dc-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
@@ -42,95 +39,21 @@ const data = [
     alt: "A Sonoran desert toad is placed on its back, a position that calms it down",
     src: "https://static01.nyt.com/images/2022/03/09/us/00toad04/merlin_203413101_9fac7969-60bb-4867-983c-93b25ea7402c-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
   },
-
   {
     alt: "Exhaling smoke after taking venom through a pipe during the ceremony. Users often call the substance the “God molecule,” likening its use to a religious experience",
     src: "https://static01.nyt.com/images/2022/03/09/us/00toad14/merlin_203377491_0e42a9d1-88aa-4a79-bd70-3c46a8e5fe76-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
   },
   {
-    alt: "A ceremony participant. The substance 5-MeO-DMT in the toad secretions remains effectively illegal in the United States, but law enforcement agencies are largely tolerating venom ceremonies. The substance is legal in Mexico. ",
+    alt: "A ceremony participant. The substance 5-MeO-DMT in the toad secretions remains effectively illegal in the United States, but law enforcement agencies are largely tolerating venom ceremonies. The substance is legal in Mexico",
     src: "https://static01.nyt.com/images/2022/03/09/us/00toad15/merlin_203377959_4e628892-c54d-4356-b378-e7adf3483ddc-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
   },
   {
     alt: "Before the ceremony, a cleansing with incense",
     src: "https://static01.nyt.com/images/2022/03/09/us/00toad10/merlin_203377371_4b58a8d9-cd18-41de-9ca9-9a93ed7c64c7-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
   },
-
   {
-    alt: "A view in Tuscon, Ariz., where the toad hibernates underground most of the year and resurfaces to breed around the summer monsoon rains. ",
+    alt: "A view in Tuscon, Ariz., where the toad hibernates underground most of the year and resurfaces to breed around the summer monsoon rains",
     src: "https://static01.nyt.com/images/2022/03/09/us/00toad05/merlin_203413209_2b064bc8-9f8b-4647-b52d-7196ffb0043e-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
-  },
-  {
-    alt: "The New Yorker",
-    src: "https://www.newyorker.com/verso/static/the-new-yorker/assets/logo.f1893bac6dafe13d6d5bad671a5bee2345efa44d.svg",
-  },
-  {
-    alt: "The New Yorker",
-    src: "https://www.newyorker.com/verso/static/the-new-yorker/assets/logo-header.6e34c81346bc43475ffd572e6c2eb3e125927148.svg",
-  },
-  {
-    alt: "​​​​​​The building of the National Academy of State Administration of the President of Ukraine was destroyed by a..",
-    src: "https://media.newyorker.com/photos/6238f654a25c316f67448407/4:3/w_1600%2Cc_limit/Gesse-PB-Karkiv-Lead.jpg",
-  },
-  {
-    alt: "Illustration of Ukrainian flag made of code",
-    src: "https://media.newyorker.com/photos/623a1413d2edfd75e5c5c7d5/4:3/w_1600%2Cc_limit/halpern_ukraine_hack.gif",
-  },
-  {
-    alt: "A group of women walking around wearing animal prints",
-    src: "https://media.newyorker.com/photos/623372bbff58350cfec3d56d/4:3/w_1600%2Cc_limit/220328_r40099.jpg",
-  },
-  {
-    alt: "U.S. Supreme Court nominee Judge Ketanji Brown Jackson is swornin during her confirmation hearing before the Senate..",
-    src: "https://media.newyorker.com/photos/62390a4ce8d3220acef749f4/4:3/w_1600%2Cc_limit/Cassidy-Jackson-Hearing.jpg",
-  },
-  {
-    alt: "Russian President Vladimir Putin speaks very closely with Defence Minister Sergei Shoigu",
-    src: "https://media.newyorker.com/photos/6239e0abdc3ee2f5bcd3110a/4:3/w_1600%2Cc_limit/Chotiner-UkrainePutinInnerCircle.jpg",
-  },
-  {
-    alt: "Illustration of a figure walking through a door made from a DNA strand",
-    src: "https://media.newyorker.com/photos/6233bca7d27f7ca57c545982/4:3/w_1600%2Cc_limit/Khullar_Sicklecells.jpg",
-  },
-  {
-    alt: "Nighttime still from Drive My Car of Hidetoshi Nishijima and Tōko Miura",
-    src: "https://media.newyorker.com/photos/6238efe5c4b9b309cd802042/4:3/w_1600%2Cc_limit/Brody-Oscar-Nominations-Drive-my-Car.jpg",
-  },
-  {
-    alt: "Illustration of bullets buried in a landscape",
-    src: "https://media.newyorker.com/photos/6234ec55610893eb03394355/4:3/w_1600%2Cc_limit/Kurkov_archeology_of_war_final.jpg",
-  },
-  {
-    alt: "Many hands pointing at one person",
-    src: "https://media.newyorker.com/photos/623219bd3fd359653b430363/4:3/w_1600%2Cc_limit/220328_r40108web-tout.jpg",
-  },
-  {
-    alt: "The New Yorker's Name Drop logo",
-    src: "https://media.newyorker.com/photos/60ff206ac954fbee507bb423/4:3/w_1600%2Cc_limit/logo-full.png",
-  },
-  {
-    alt: "Eustace holding a pencil to a blank crossword puzzle",
-    src: "https://media.newyorker.com/photos/610b015983948a74303d15d4/4:3/w_1600%2Cc_limit/EustaceCrossword-WE5-4x3.jpg",
-  },
-  {
-    alt: "Eustace dressed like Sherlock Holmes holding a pencil to a blank crossword puzzle",
-    src: "https://media.newyorker.com/photos/610b018b58fdd722890e6973/4:3/w_1600%2Cc_limit/EustaceCrossword-Cryptic-4x3.jpg",
-  },
-  {
-    alt: "A pencil holding a person upside down on a piece of paper",
-    src: "https://media.newyorker.com/photos/610b01157f482b0f3d8f07e4/4:3/w_1600%2Cc_limit/caption-contest-thumb.jpg",
-  },
-  {
-    alt: "Jazz ensemble in the background woman in front singing with arms wide open",
-    src: "https://media.newyorker.com/photos/62336867a25c316f67447c8d/3:4/w_1600%2Cc_limit/2022_03_28.jpg",
-  },
-  {
-    alt: "A Soldier Goes Missing in Ukraine—Can His Father Find Him?",
-    src: "https://dwgyu36up6iuz.cloudfront.net/heru80fdn/image/upload/c_fill%2Cd_placeholder_image.jpg%2Cfl_progressive%2Cg_center%2Ch_900%2Cq_80%2Cw_1600/v1646164642/thenewyorker_i-never-had-dreams-of-my-son.jpg",
-  },
-  {
-    alt: "Sticks! What Can’t They Do?",
-    src: "https://dwgyu36up6iuz.cloudfront.net/heru80fdn/image/upload/c_fill%2Cd_placeholder_image.jpg%2Cfl_progressive%2Cg_center%2Ch_900%2Cq_80%2Cw_1600/v1645641744/thenewyorker_stick-world.jpg",
   },
   {
     alt: "The New Yorker",
@@ -147,5 +70,9 @@ const data = [
   {
     alt: "dot image pixel",
     src: "https://sp.analytics.yahoo.com/sp.pl?a=10000&b=The%20New%20Yorker&.yp=10155331&f=https%3A%2F%2Fwww.newyorker.com%2F&enc=UTF-8&us_privacy=1---&yv=1.12.0&et=custom&ea=pageview&tagmgr=gtm",
+  },
+  {
+    alt: "Google",
+    src: "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
   },
 ];
